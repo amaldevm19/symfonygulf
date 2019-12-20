b452851195f9169ab0f4db11f5c50fb5c5a66b96
amaldevm19 <amaldevm19@gmail.com>
2019-12-20 14:48:30 +0400

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 (function($){
   $(function(){
-
     $('.sidenav').sidenav({
       draggable:true
     });
@@ -8,38 +7,20 @@
     $(".dropdown-trigger").dropdown();
     $('#carousel').carousel({dist:50});
     $('#mobcarousel').carousel({dist:150});
-    $('#mycarasoul').carousel({
-      //dist: -100,
-      //autoplay: true,
-      //numVisible:4,
-      padding:200,
-      //fullWidth:true
-    });
-    $('#mobileWhoWeAre').click((e)=>{
-      e.preventDefault();
-      console.log("Hello");
-    });
+    $('#mycarasoul').carousel({padding:200});
     $(document).ready(function(){
       $('.materialboxed').materialbox({
         onOpenStart : clearCarosoulTimeout,
         onCloseEnd : autoplay
       });
     });
-
-    $(document).ready(function() {
-      $('textarea#textarea2').characterCounter();
-    });
-    //$('#mycarasoul').carousel({
-     // padding: 0,
-     // numVisible:6    
-      //});
-  autoplay();
-  var tim;
-  var clearCarosoulTimeout = ()=> clearTimeout(tim);
-  function autoplay() {
+    $('textarea#textarea2').characterCounter();
+    autoplay();
+    var tim;
+    var clearCarosoulTimeout = ()=> clearTimeout(tim);
+    function autoplay() {
       $('#mycarasoul').carousel('next');
-      tim = setTimeout(autoplay, 4000); 
-          
+      tim = setTimeout(autoplay, 4000);     
       }
   }); // end of document ready
 })(jQuery); // end of jQuery name space
